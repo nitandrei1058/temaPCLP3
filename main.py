6eeecfd9d78eece22fe4bf00c11957b9e97f84c2
nitandrei1058 <152653215+nitandrei1058@users.noreply.github.com>
2024-05-30 21:35:03 +0300

    Task1

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ import matplotlib.ticker as mticker
 matplotlib.use('TkAgg')
 file = 'train.csv'
 
+def task1(data):
+    n = len(data.columns)
+    m = len(data)
+    types = data.dtypes
+    miss = data.isnull().sum()
+    dup = data.duplicated().sum()
+    print(f'Number of columns: {n}\n')
+    print(f'Data types:\n')
+    print(f'{types}\n')
+    print(f'Missing values:')
+    print(f'{miss}\n')
+    print(f'Number of rows: {m}\n')
+    print(f'Duplicates: {dup}')
+
+
+
 
 
 
