5f704af1ba5c1165e78096328d96b2ec83c6346b
nitandrei1058 <152653215+nitandrei1058@users.noreply.github.com>
2024-05-30 21:35:14 +0300

    Task 2

--- main.py
+++ main.py
@@ -19,6 +19,32 @@ def task1(data):
     print(f'Number of rows: {m}\n')
     print(f'Duplicates: {dup}')
 
+def task2(data):
+    surv = data['Survived'].value_counts(normalize=True) * 100
+    pcls = data['Pclass'].value_counts(normalize=True) * 100
+    sex = data['Sex'].value_counts(normalize=True) * 100
+    print(f'How many survived: {surv[1]}%')
+    print(f'How many did not survive: {surv[0]}%\n')
+    print(f'Passengers Class:')
+    for key, value in pcls.items():
+        print(f'{key}: {value}%')
+    print(f"\nSex:")
+    for key, value in sex.items():
+        print(f'{key}: {value}%')
+    
+    _, plot = plt.subplots(3, 1)
+    plot[0].set_title('Survival rate')
+    plot[1].set_title('Passenger Class')
+    plot[2].set_title('Sex')
+
+    plot[0].bar(['Did not survive', 'Survived'], surv)
+    plot[1].set_xticks(pcls.index)
+    plot[1].bar(pcls.index, pcls)
+    plot[2].bar(sex.index, sex)
+
+    plt.tight_layout()
+    plt.show()
+
 
 
 
