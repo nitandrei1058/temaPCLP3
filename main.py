0ba2c36760af32b1c2f15d50df6892b6c260f062
nitandrei1058 <152653215+nitandrei1058@users.noreply.github.com>
2024-05-30 21:34:38 +0300

    added main body

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+matplotlib.use('TkAgg')
+file = 'train.csv'
+
+
+
+
+
+
+
+def main():
+    data = pd.read_csv(file)
+
+
+if __name__ == '__main__':
+    main()
